0899ce1aff3e1cbdb3d5b328f32b8fada9b96080
Sannidhi-Sriram <152961245+Sannidhi-Sriram@users.noreply.github.com>
2024-03-01 01:06:29 +0530

    Update web scraping.py

--- web scraping.py
+++ web scraping.py
@@ -1,14 +1,8 @@
 # This is the first piece of program from our project we have been researching about different websites to get the most effective dataset to include in our project
 import requests 
 from bs4 import BeautifulSoup 
-  
-  
-# Making a GET request 
 r = requests.get('https://www.tensorflow.org/api_docs/python/tf') 
-  
-# Parsing the HTML 
 soup = BeautifulSoup(r.content, 'html.parser') 
-  
 s = soup.find('section') 
 content = s.find_all('a') 
 for line in content:
